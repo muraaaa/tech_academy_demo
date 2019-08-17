bf117d8c2d2c2085c651b6984b535699be81737a
tm <tm@tms-MacBook-Air.local>
2019-08-17 18:58:07 +0900

    header-finished campaign-ongoing

--- src/App.vue
+++ src/App.vue
@@ -75,11 +75,240 @@
                   <li class="dropdown btn-nav-menu"><a class="link" href="#">セット</a></li>
                   <li class="dropdown btn-nav-menu"><a class="link" href="#">はじめての方へ</a></li>
                   <li class="dropdown btn-nav-menu"><a class="link" href="#">転職サポート</a></li>
-                  <li class="dropdown btn-nav-menu"><a class="link" href="#">法人のお客様</a></li>
+                  <li class="dropdown btn-nav-menu"><a class="link for-company" href="#">法人のお客様</a></li>
                   <li class="dropdown btn-nav-menu"><a class="link log-in" href="#">ログイン</a></li>
               </ul> 
           </div>
       </div>
+
+    <!-- 海の日キャンペーン -->
+      <section id="campaign">
+        <img src="⁨./assets/title.png" alt="キャンペーン">
+        <!--  質問：画像が反映されないのは何故？-->
+        <p class="campaign-text">
+            ブートキャンプの8・１２・16・24週間プランのいずれかに
+            お申し込みいただくと、<br>もれなく「はじめてのプログラミングコース」のカリキュラムをプレゼント！<br>海の日までのお得なキャンペーンです。
+        </p>
+        <p class="reminder">残りわずか！お早めに</p>
+        <a class="campaigned-course" href="#">対象となるコースを見る</a>
+      </section> 
+
+     <!-- 受賞 -->
+    <section class="advs">
+        <div class="award">
+            <img src="img/awards.png" alt="受賞">
+            <p>第１３回日本e-learning大賞<br>プログラミング教育特別部門賞</p>
+        </div>
+        <div class="payment">
+            <img src="img/bitcoin.png" alt="bit-coin">
+            <p>ビットコインで<br>お支払いただけます</p>
+        </div>
+    </section>
+
+
+    <!-- 無料キャンペーン -->
+    <section class="free-of-charge">
+        <a href="#" class="foc-topics">
+            <div class="topic">無料体験</div>
+            <img src="/img/無料体験.jpg" alt="無料体験">
+            <div class="content">
+                <h2>ブートキャンプを無料体験してみよう！</h2>
+                <p>
+                    実際の学習システムやカリキュラムをご覧いただけます。
+                    受講をお考えの方はぜひご体験を！
+                </p>
+            </div>
+        </a>
+        <a href="#" class="foc-topics">
+            <div class="topic">無料体験</div>
+            <img src="/img/コース選び.jpg" alt="コース選び">
+            <div class="content">
+                <h2>受講の目的別にオススメコースを紹介</h2>
+                <p>
+                    どのコースがあっているかわからない！そんなあなたにオススメの
+                    コースを紹介します。
+                </p>
+            </div>
+        </a>
+        <a href="#" class="foc-topics">
+            <div class="topic">無料体験</div>
+            <img src="/img/転職.jpg" alt="転職">
+            <div class="content">
+                <h2>未経験から12週間でエンジニアを目指す</h2>
+                <p>
+                    プログラミングを習得して転職が決まらなければ授業料を
+                    全額返金する保証あり。
+                </p>
+            </div>
+        </a>
+        <a href="#" class="foc-topics">
+            <div class="topic">無料体験</div>
+            <img src="/img/受け放題.jpg" alt="受け放題">
+            <div class="content">
+                <h2>コースを受講し放題！最大160万円相当お得！</h2>
+                <p>
+                    受け放題はブートキャンプのコースを24時間受講し放題！
+                    今ならメンタリング８回分が無料！
+                </p>
+            </div>
+        </a>
+    </section>
+
+    <!-- Trial -->
+    <section class="trial">
+        <img src="/img/top-trial-ill-0b8d6a09a9ee2ae66bc314cd954b019fade0a94471be104a0dcd855cad1ce896.png" alt="">
+        <div class="explanation">
+            <h2 class="section-title"><span>プログラミング無料体験</span>やってます</h2>
+            <p>メンターサポートの体験・カリキュラムの閲覧・プログラミングの体験</p>
+        </div>
+        <button>詳しく見る</button>
+    </section>
+
+
+    <!-- ブートキャンプ -->
+    <section class="boot-camp">
+        <div class="boot-camp-container">
+            <h2 class="title">ブートキャンプ</h2>
+            <p>現役のプロのマンツーマンメンタリングで実践的スキルを習得</p>
+        </div>
+        <section class="sub-section">
+            <p class="title-border">プログラミング</p>
+            <div class="courses">
+                <a href="#" class="course Ruby">
+                    <div class="course-wrapper">
+                        <div class="course-container">
+                            <i class="fa fa-diamond"></i>
+                            <h3>Webアプリケーションコース</h3>
+                        </div>
+                    </div>
+                    <div class="deadline">
+                        <p>申込締切（本日締切）<br><span>7</span>月<span>21</span>日26時</p>
+                    </div>
+                </a>
+                <a href="#" class="course PHP">
+                    <div class="course-wrapper">
+                        <div class="course-container">
+                            <i class="fa fa-diamond"></i>
+                            <h3>PHP/Laravelコース</h3>
+                        </div>
+                    </div>
+                    <div class="deadline">
+                        <p>申込締切（本日締切）<br><span>7</span>月<span>21</span>日26時</p>
+                    </div>
+                </a>
+                <a href="#" class="course Java">
+                    <div class="course-wrapper">
+                        <div class="course-container">
+                            <i class="fa fa-diamond"></i>
+                            <h3>Javaコース</h3>
+                        </div>
+                    </div>
+                    <div class="deadline">
+                        <p>申込締切（本日締切）<br><span>7</span>月<span>21</span>日26時</p>
+                    </div>
+                </a>
+                <a href="#" class="course HTML">
+                    <div class="course-wrapper">
+                        <div class="course-container">
+                            <i class="fa fa-diamond"></i>
+                            <h3>フロントエンドコース</h3>
+                        </div>
+                    </div>
+                    <div class="deadline">
+                        <p>申込締切（本日締切）<br><span>7</span>月<span>21</span>日26時</p>
+                    </div>
+                </a>
+            </div>
+        </section>
+    </section>
+
+
+    <!-- Techacadyとは -->
+    <section id="abstract">
+        <h2 class="title">TechAcademyとは？</h2>
+        <p>
+            プログラミングやアプリ開発を学べるオンラインスクール。<br>
+            1人では続かない方のための短期集中プログラム「オンラインブートキャンプ」を開催。<br>
+            現役のプロのサポートと独自の学習システムで短期間で成長いただけます。
+        </p>
+        <p>600社、30,000名を超える教育実績</p>
+        <img src="/img/company-logos.jpg" alt="company logo">
+        <div class="iframe-wrapper">
+            <a href="#">
+                <iframe  src="https://www.youtube.com/embed/XwJoiznVrps" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <!-- width="560" height="315" -->
+                <div class="cm-text">
+                    <div class="cm-text-title">
+                        <h2>TechAcademyのテレビCM<br>「未来の自分編」</h2>
+                    </div>
+                    <div class="cm-text-detail">
+                        <p>現在と未来の自分が繰り広げる会話劇を是非ご覧ください</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </section>
+
+
+
+<!-- 他スクールとの比較 -->
+    <section id="comparison">
+        <h2>他スクールとの比較</h2>
+        <p>他のスクールと比較するとTechAcademyの<br>
+            圧倒的なコストパフォーマンスをご確認いただけます</p>
+        <table border="1" width="93%" align="center">
+            <thead>
+                <tr height="100">
+                    <th width="18%" ></th>
+                    <th width="21.5%" class="TechAcademy-related top"><img src="/img/logo-8511a75ca8fe7f18d0d46acfd2d0eb51b7b5a5c5786e12a1c6781f64bbbd0e8a.png" alt="TechAcademy"></th>
+                    <th width="19.5%">A社</th>
+                    <th width="19.5%">B社</th>
+                    <th width="19.5%">C社</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr height="100" >
+                    <td>料金</td>
+                    <td  class="TechAcademy-related">99,000円〜<br><span>※はじめてのプログラミングコース4週間の料金</span></td>
+                    <td>148,000円</td>
+                    <td>入会金128,000円<br>月額12,800円</td>
+                    <td>入会金18,000円<br>月額96,000円</td>
+                </tr>
+                <tr height="100" >
+                    <td>マンツーマンレッスン<br>メンタリング</td>
+                    <td class="TechAcademy-related column"><i class="fa fa-circle-o" aria-hidden="true"></i></td>
+                    <td><i class="fa fa-circle-o" aria-hidden="true"></i></td>
+                    <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                    <td><i class="fa fa-circle-o" aria-hidden="true"></i></td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+    
+
+    <!-- footer -->
+    <footer id="footer">
+        <!-- <img src="/img/media-logos.png" alt="media-logo"> -->
+        <div class="footer-container">
+            <div class="footer-menu">
+                <ul>
+                    <li><a href="#">お問い合わせ</a></li>
+                    <li><a href="#">よくあるご質問</a></li>
+                </ul>
+            </div>
+            <div class="company-details">
+                <ul>
+                    <li><a href="#">運営会社</a></li>
+                    <li><a href="#">利用規約</a></li>
+                </ul>
+                <div class="social-network">
+                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
   </header>    
 </template>
 
@@ -94,4 +323,5 @@ export default {
 @import './assets/style/reset';
 @import './assets/style/common';
 @import './assets/style/header';
+@import './assets/style/campaign';
 </style>
